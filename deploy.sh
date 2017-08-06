cd9eb8b4caced65d75ff9953d2f79c7014e08af7
Merrill Lines <linesmerrill@gmail.com>
2017-08-06 08:46:02 -0700

    modify main page content

--- deploy.sh
+++ deploy.sh
@@ -1,20 +1,30 @@
-#!/bin/bash
 
-echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
+#!/bin/sh
 
-# Build the project.
+DIR=$(dirname "$0")
+
+cd $DIR/..
+
+if [[ $(git status -s) ]]
+then
+    echo "The working directory is dirty. Please commit any pending changes."
+    exit 1;
+fi
+
+echo "Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "Checking out gh-pages branch into public"
+git worktree add -B gh-pages public origin/gh-pages
+
+echo "Removing existing files"
+rm -rf public/*
+
+echo "Generating site"
 hugo
 
-# Add changes to git.
-git add -A
-
-# Commit changes.
-msg="rebuilding site `date`"
-if [ $# -eq 1 ]
-  then msg="$1"
-fi
-git commit -m "$msg"
-
-# Push source and build repos.
-git push origin master
-git subtree push --prefix=public https://github.com/linesmerrill/merrilllines.git gh-pages
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)"
