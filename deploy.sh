088ead92ea86e8f66c9e78ef9d61651a33fa219c
Merrill Lines <linesmerrill@gmail.com>
2018-07-24 19:23:24 -0700

    update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,31 +10,20 @@ fi
 echo "Deleting old publication"
 rm -rf public
 mkdir public
-#git worktree prune
-#rm -rf .git/worktrees/public/
+git worktree prune
+rm -rf .git/worktrees/public/
 
-echo "clone gh-pages branch from the local repo into a repo located within \"public\""
-git clone .git --branch gh-pages public
-
-echo "generate"
-hugo
-
-# commit the changes in the clone and push them back to the local gh-pages branch    
-cd public && git add --all && git commit -m "Publishing to gh-pages" && git push origin gh-pages
-
-# publish
-
-#echo "Checking out gh-pages branch into public"
+echo "Checking out gh-pages branch into public"
 git worktree add -B gh-pages public origin/gh-pages
 
-#echo "Removing existing files"
-#rm -rf public/*
+echo "Removing existing files"
+rm -rf public/*
 
-#echo "Generating site"
-#hugo
+echo "Generating site"
+hugo
 
-#echo "Updating gh-pages branch"
-#cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && cd ..
+echo "Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (publish.sh)" && cd ..
 
-#echo "Updating master branch"
+echo "Updating master branch"
 git push
